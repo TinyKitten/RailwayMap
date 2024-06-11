9267e57cf0d570980670579848172d73480f301c
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-06-11 10:00:33 +0900

    mb-1

--- components/Credit.tsx
+++ components/Credit.tsx
@@ -3,7 +3,7 @@ import { AppLogo } from "./AppLogo";
 
 export const Credit: React.FC = () => (
   <footer className="fixed right-[32px] bottom-[48px] flex flex-col items-end opacity-50">
-    <AppLogo className="mb-2" width={127.5} height={38.4} />
+    <AppLogo className="mb-1" width={127.5} height={38.4} />
     <p className="text-xs margin-0 select-none">
       A product of{" "}
       <a
