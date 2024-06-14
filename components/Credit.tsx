4df55049aa4f67b3d246299b7377ba2902227216
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-06-14 20:18:49 +0900

    ウォーターマーク小さめ薄くなくていい

--- components/Credit.tsx
+++ components/Credit.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { AppLogo } from "./AppLogo";
 
 export const Credit: React.FC = () => (
-  <footer className="fixed right-[32px] bottom-[48px] flex flex-col items-end opacity-40">
-    <AppLogo className="w-32 h-auto" />
-    <p className="text-xs margin-0 select-none">
+  <footer className="fixed right-[32px] bottom-[48px] flex flex-col items-end opacity-50">
+    <AppLogo className="w-24 h-auto" />
+    <p className="text-[8px] margin-0 select-none">
       A product of{" "}
       <a
-        className="no-underline text-xs font-bold outline-none"
+        className="no-underline font-bold outline-none"
         target="_blank"
         rel="noreferrer noopener"
         href="https://tinykitten.me"
